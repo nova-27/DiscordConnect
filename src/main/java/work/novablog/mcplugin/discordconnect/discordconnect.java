02d54d5b88b2b4c08e18212e1ff45a17492ae67d
BuildTools <unconfigured@null.spigotmc.org>
2021-02-28 22:21:51 +0900

    Initial Commit

---
+++ src/main/java/work/novablog/mcplugin/discordconnect/discordconnect.java
@@ -0,0 +1,16 @@
+package work.novablog.mcplugin.discordconnect;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class discordconnect extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
