06d2329dc463c5db439f0e9e2513251537094a1b
BuildTools <unconfigured@null.spigotmc.org>
2021-03-01 22:04:56 +0900

    [add]JDAボットを処理追加

--- src/main/java/work/novablog/mcplugin/discordconnect/DiscordConnect.java
+++ src/main/java/work/novablog/mcplugin/discordconnect/DiscordConnect.java
@@ -1,16 +1,31 @@
 package work.novablog.mcplugin.discordconnect;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.dv8tion.jda.api.JDA;
+import work.novablog.mcplugin.discordconnect.listener.DiscordListener;
+
+import javax.security.auth.login.LoginException;
 
 public final class DiscordConnect extends Plugin {
+    String TOKEN = "";
+    JDA bot;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        try {
+            bot = JDABuilder.createDefault(TOKEN)
+                    .setActivity(Activity.playing("name"))
+                    .addEventListeners(new DiscordListener())
+                    .build();
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        bot.shutdown();
     }
 }
