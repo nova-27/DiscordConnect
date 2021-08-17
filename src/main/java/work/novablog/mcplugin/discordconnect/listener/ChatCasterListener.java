a44c532ede61b976e805028ccda7e6d7e5e5c9c3
nova-27 <yutodes0305@gmail.com>
2021-08-17 23:31:17 +0900

    [fix]ChatCasterをWebhookに対応

--- src/main/java/work/novablog/mcplugin/discordconnect/listener/ChatCasterListener.java
+++ src/main/java/work/novablog/mcplugin/discordconnect/listener/ChatCasterListener.java
@@ -6,6 +6,7 @@ import com.gmail.necnionch.myplugin.n8chatcaster.bungee.events.GlobalChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 import work.novablog.mcplugin.discordconnect.DiscordConnect;
+import work.novablog.mcplugin.discordconnect.util.ConvertUtil;
 
 public class ChatCasterListener implements Listener {
     /**
@@ -21,6 +22,14 @@ public class ChatCasterListener implements Listener {
         String message = DiscordConnect.getInstance().getChatCasterAPI().formatMessageForDiscord(event);
         MarkComponent[] components = MarkdownConverter.fromMinecraftMessage(message, '&');
         String output = MarkdownConverter.toDiscordMessage(components);
-        DiscordConnect.getInstance().getBotManager().sendMessageToChatChannel(output);
+
+        String name = event.getSender().getName();
+        String avatarURL = ConvertUtil.getMinecraftAvatarURL(event.getSender().getUniqueId());
+
+        DiscordConnect.getInstance().getDiscordWebhookSenders().forEach(sender -> sender.sendMessage(
+                name,
+                avatarURL,
+                output
+        ));
     }
 }
